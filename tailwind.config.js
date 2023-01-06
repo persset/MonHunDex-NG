72fc8c96525fd041be24a8ab8eb1f18b80f7627b
Jose Victor Persset <jpersset@gmail.com>
2023-01-06 13:34:54 -0300

    Start wrapping home page template

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "placeholder-bgi":
+          "url('https://images3.alphacoders.com/251/251305.jpg')",
+      },
+    },
   },
   plugins: [],
 };
