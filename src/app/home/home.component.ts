e4e38ca0cbe5433a1472d08ffd2bec254cae9bb2
Jose Victor Persset <jpersset@gmail.com>
2023-01-09 11:07:23 -0300

    Create tab to change the displayed info for the  daily monster

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  openMonsterInfo(infoName: String) {
+    let elements: HTMLCollectionOf<Element> =
+      document.getElementsByClassName('dm-info');
+    for (let i: number = 0; i < elements.length; i++) {
+      elements[i].setAttribute('style', 'display: none');
+    }
 
+    document
+      .getElementById(infoName.valueOf())
+      ?.setAttribute('style', 'display:block');
+  }
 }
