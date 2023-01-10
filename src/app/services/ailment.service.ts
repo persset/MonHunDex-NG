e5858caa5af5013ae420d3cc5cd0a84a9c8c36dc
Jose Victor Persset <jpersset@gmail.com>
2023-01-10 10:21:20 -0300

    Generate 'Ailment' service

---
+++ src/app/services/ailment.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Ailment } from '../models/Ailment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AilmentService {
+  private url = 'Ailment';
+  constructor(private http: HttpClient) {}
+
+  public GetAllAilments(): Observable<Ailment[]> {
+    return this.http.get<Ailment[]>(`${environment.apiUrl}/${this.url}`);
+  }
+
+  public getSingleAilment(ailment: Ailment): Observable<Ailment> {
+    return this.http.get<Ailment>(
+      `${environment.apiUrl}/${this.url}/${ailment.id}`
+    );
+  }
+}
