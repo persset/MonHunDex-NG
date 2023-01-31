2d52e65f14826f44637425c28431f566efbce67e
Jose Victor Persset <jpersset@gmail.com>
2023-01-31 11:12:16 -0300

    Consume api to get list of monsters on database

--- src/app/monsters/monsters.component.ts
+++ src/app/monsters/monsters.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { isConstructorDeclaration } from 'typescript';
+import { Monster } from '../models/Monster';
+import { MonsterService } from '../services/monster.service';
 
 @Component({
   selector: 'app-monsters',
   templateUrl: './monsters.component.html',
-  styleUrls: ['./monsters.component.css']
+  styleUrls: ['./monsters.component.css'],
 })
 export class MonstersComponent {
+  title = 'Monsters';
+  monsters: Monster[] = [];
 
+  constructor(private monsterService: MonsterService) {}
+
+  ngOnInit(): void {
+    this.monsterService
+      .getAllMonsters()
+      .subscribe((result: Monster[]) => (this.monsters = result));
+
+    console.log(this.monsters);
+  }
 }
--- src/app/services/monster.service.ts
+++ src/app/services/monster.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Monster } from '../models/monster';
+import { Monster } from '../models/Monster';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 
